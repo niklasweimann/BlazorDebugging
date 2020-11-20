dde1e5e3b6afecdcaf7e587e7a5f947c14e31a12
Niklas Weimann <nikweimann@gmail.com>
2020-11-20 10:06:29 +0100

    Add example for null reference

---
+++ BlazorDebugging/SingeltonFoo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDebugging
+{
+    public class SingeltonFoo
+    {
+        public static SingeltonFoo Instance { get; set; } = new SingeltonFoo();
+
+        public delegate void MyEventHandler(string foo);
+
+        public event MyEventHandler SomethingHappened;
+
+        public void OnSomethingHappend(string foo)
+        {
+            SomethingHappened?.Invoke(foo);
+        }
+        private SingeltonFoo()
+        {
+
+        }
+    }
+}
